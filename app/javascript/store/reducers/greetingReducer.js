3a9e447eb149ba0f86377cda333304d5950c04bb
Matee <matiullahsafibbc@gmail.com>
2023-09-21 20:42:41 +0430

    setup webpack

---
+++ app/javascript/store/reducers/greetingReducer.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const greetingSlice = createSlice({
+
+    name: 'greeting',
+
+    initialState: {
+
+        content: '',
+
+    },
+
+    reducers: {
+
+        setGreeting: (state, action) => {
+
+          state.content = action.payload;
+
+        },
+
+    },
+
+})
+
+
+
+
+export const { setGreeting } = greetingSlice.actions;
+
+export default greetingSlice.reducer;
